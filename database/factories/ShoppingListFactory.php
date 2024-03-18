3134ff2e0610b06b88cf0aff07ca243c8f9c6a4b
Bruno Pithon <brunopithon@velty.com.br>
2024-03-17 21:15:41 -0300

    Criando o Factory de ShoppingList para usar nos testes

---
+++ database/factories/ShoppingListFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ShoppingList>
+ */
+class ShoppingListFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence(3), 
+            'description' => $this->faker->paragraph(2), 
+            // 'user_id' => User::factory(),
+        ];
+    }
+}
