1845cba0fb94a2123fda41dc2d0b76deb1364ba6
Bruno Pithon <brunopithondev@gmail.com>
2024-07-14 18:08:11 -0300

    Incluindo o método showItems em ShoppingListController

--- app/Http/Controllers/ShoppingListController.php
+++ app/Http/Controllers/ShoppingListController.php
@@ -7,46 +7,59 @@ use App\Models\ShoppingList;
 
 class ShoppingListController extends Controller
 {
-    public function __construct(private ShoppingList $shoppingList)
-    {
+  public function __construct(private ShoppingList $shoppingList)
+  {
 
+  }
+
+  public function showItems($shopping_list_id)
+  {
+    $shoppingList = $this->shoppingList->find($shopping_list_id);
+
+    if (!$shoppingList) {
+      return response()->json(['message' => 'Shopping list not found.'], 404);
     }
 
-    public function index()
-    {
-        return response()->json($this->shoppingList->all());
+    $items = $shoppingList->items;
+
+    return response()->json($items);
+  }
+
+  public function index()
+  {
+    return response()->json($this->shoppingList->all());
+  }
+
+  public function show($id)
+  {
+    $shoppingList = $this->shoppingList->find($id);
+    return response()->json($shoppingList);
+  }
+
+  public function store(Request $request)
+  {
+    $shoppingList = $this->shoppingList->create($request->all());
+    return response()->json($shoppingList, 201);
+  }
+
+  public function update(Request $request, $id)
+  {
+    $shoppingList = $this->shoppingList->find($id);
+
+    if (!$shoppingList) {
+      return response()->json(['message' => 'Shopping list not found.'], 404);
     }
 
-    public function show($id)
-    {
-        $shoppingList = $this->shoppingList->find($id);
-        return response()->json($shoppingList);
-    }
+    $shoppingList->update($request->all());
 
-    public function store(Request $request)
-    {
-        $shoppingList = $this->shoppingList->create($request->all());
-        return response()->json($shoppingList, 201);
-    }
+    return response()->json($shoppingList, 200);
+  }
 
-    public function update(Request $request, $id)
-    {
-        $shoppingList = $this->shoppingList->find($id);
+  public function destroy($id)
+  {
+    $shoppingList =$this->shoppingList->find($id);
+    $shoppingList->delete();
 
-        if (!$shoppingList) {
-            return response()->json(['message' => 'Shopping list not found.'], 404);
-        }
-
-        $shoppingList->update($request->all());
-
-        return response()->json($shoppingList, 200);
-    }
-
-    public function destroy($id)
-    {
-        $shoppingList =$this->shoppingList->find($id);
-        $shoppingList->delete();
-
-        return response()->json([], 204);
-    }
+    return response()->json([], 204);
+  }
 }
--- app/Models/ShoppingList.php
+++ app/Models/ShoppingList.php
@@ -7,10 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShoppingList extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $fillable = [
-        'name',
-        'description'
-    ];
+  protected $fillable = [
+    'name',
+    'description'
+  ];
+
+  public function items()
+  {
+    return $this->hasMany(Item::class);
+  }
 }
